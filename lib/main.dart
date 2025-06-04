e006bbc4eff057c02dbecd03654689bdce39aadf
susan040 <susanthapa040@gmail.com>
2025-06-04 13:39:27 +0545

    added app launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:easy_world_vendor/controller/theme_controller.dart';
 import 'package:easy_world_vendor/views/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await GetStorage.init();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final ThemeController themeController = Get.put(ThemeController());
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +19,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
-      // themeMode: themeController.theme,
+      themeMode: themeController.theme,
       home: SplashScreen(),
     );
   }
