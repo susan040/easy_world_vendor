dcf86382533741fc688f5a1a15b05d58ad0b5785
susan040 <susanthapa040@gmail.com>
2025-06-26 14:12:35 +0545

    update the PDF page

--- lib/views/profile/pdf_view_screen.dart
+++ lib/views/profile/pdf_view_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
 import 'package:easy_world_vendor/utils/colors.dart';
 import 'package:easy_world_vendor/utils/custom_text_style.dart';
 import 'package:get/get.dart';
@@ -9,11 +12,22 @@ class PdfViewScreen extends StatelessWidget {
 
   const PdfViewScreen({super.key, required this.pdfUrl});
 
+  Future<String> _downloadAndSavePdf(String url) async {
+    final response = await http.get(Uri.parse(url));
+    final bytes = response.bodyBytes;
+
+    final dir = await getTemporaryDirectory();
+    final file = File('${dir.path}/temp.pdf');
+    await file.writeAsBytes(bytes);
+    return file.path;
+  }
+
   @override
   Widget build(BuildContext context) {
     final isDark = Theme.of(context).brightness == Brightness.dark;
     final bgColor = isDark ? AppColors.darkModeColor : AppColors.extraWhite;
     final textColor = isDark ? AppColors.extraWhite : AppColors.blackColor;
+
     return Scaffold(
       backgroundColor: bgColor,
       appBar: AppBar(
@@ -29,9 +43,31 @@ class PdfViewScreen extends StatelessWidget {
           style: CustomTextStyles.f16W700(color: textColor),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(right: 16, left: 16),
-        child: SfPdfViewer.network(pdfUrl),
+      body: FutureBuilder<String>(
+        future: _downloadAndSavePdf(pdfUrl),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Failed to load PDF'));
+          }
+
+          final path = snapshot.data!;
+          return Container(
+            color: bgColor,
+
+            child: PDFView(
+              filePath: path,
+              autoSpacing: true,
+              swipeHorizontal: false,
+              pageSnap: true,
+              fitPolicy: FitPolicy.BOTH,
+              fitEachPage: true,
+              backgroundColor:
+                  isDark ? AppColors.darkModeColor : AppColors.extraWhite,
+            ),
+          );
+        },
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import file_selector_macos
 import google_sign_in_ios
 import open_file_mac
 import path_provider_foundation
+import pdfx
 import sqflite_darwin
 import syncfusion_pdfviewer_macos
 import url_launcher_macos
@@ -22,6 +23,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
